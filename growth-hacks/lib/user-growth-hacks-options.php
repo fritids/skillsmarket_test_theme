acbbebfb98e9f074b01b7f9e979afe8a351434c0
Tom Zabek <tom@thestage.co.uk>
2013-10-09 18:27:55 +0100

    Update user meta key (_growth_hack_tweet-to-unlock-color-scheme) to 'locked'

--- growth-hacks/lib/user-growth-hacks-options.php
+++ growth-hacks/lib/user-growth-hacks-options.php
@@ -5,5 +5,5 @@
 // Add options after user registration
 add_action('user_register', 'myplugin_registration_save');
 function myplugin_registration_save($user_id) {
-	add_user_meta( $user_id, '_growth_hack_tweet-to-unlock-color-scheme', 0 );
+	add_user_meta( $user_id, '_growth_hack_tweet-to-unlock-color-scheme', "locked" );
 }
