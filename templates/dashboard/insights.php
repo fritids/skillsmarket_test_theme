fcd534937c8cf427157f50c08bc403fa966b8cad
Tom Zabek <tom@thestage.co.uk>
2013-10-10 15:28:46 +0100

    Add Insights sections

--- templates/dashboard/insights.php
+++ templates/dashboard/insights.php
@@ -4,9 +4,16 @@
 
 global $wp_query, $current_user;
 
+$insights_sections = 'templates/dashboard/insights-sections-templates/';
+
 ?>
 <div id="insights-dashboard">
 	<header>
 		<h1>Insights Dashboard</h1>
 	</header>
+	<div class="dashboard-content">
+		<?php get_template_part( $insights_sections . 'teachers' ); // load Teachers ?>
+		<?php get_template_part( $insights_sections . 'students' ); // load Students ?>
+		<?php get_template_part( $insights_sections . 'classes' ); // load Students ?>
+	</div>
 </div>
